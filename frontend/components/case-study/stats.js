60abf399c784af06260525db6ac7859b2d92286e
Nicholas Siciliano <nsicilia@oswego.edu>
2023-09-13 00:06:40 -0400

    fixed wrapping project title

--- frontend/components/case-study/stats.js
+++ frontend/components/case-study/stats.js
@@ -18,7 +18,7 @@ export default function Stats({
       <div className="mx-auto max-w-6xl">
         <div className="flex flex-col mb-6 lg:flex-row md:mb-10">
           <div className="lg:w-1/2 text-center sm:text-left">
-            <h2 className="max-w-md mb-6 text-4xl font-bold tracking-tight text-white sm:text-5xl sm:leading-none xl:max-w-lg">
+            <h2 className=" mb-6 text-4xl font-bold tracking-tight text-white sm:text-5xl sm:leading-none ">
               {title}
             </h2>
           </div>
