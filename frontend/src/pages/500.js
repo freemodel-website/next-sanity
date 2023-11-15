62520c11f66d1b12dd94b3f227823570180e44b6
Nicholas Siciliano <nsicilia@oswego.edu>
2023-11-15 00:32:19 -0500

    updating 500 page

--- frontend/src/pages/500.js
+++ frontend/src/pages/500.js
@@ -1,23 +1,35 @@
-import React, { useEffect } from "react";
-import { useHistory } from "react-router-dom";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
-export default function Custom500() {
-  const history = useHistory();
+const Custom500 = () => {
+  const router = useRouter();
 
   useEffect(() => {
     // Redirect to the homepage after a delay (e.g., 3 seconds)
     const redirectTimeout = setTimeout(() => {
-      history.push("/");
-    }, 3000);
+      router.push("/");
+    }, 1000);
 
     // Clear the timeout to avoid redirection if the component unmounts
     return () => clearTimeout(redirectTimeout);
-  }, [history]);
+  }, [router]);
 
   return (
     <div>
-      <h1>500 - Server-side error occurred</h1>
-      <p>Redirecting to the homepage...</p>
+      <div class="grid h-screen px-4 bg-white place-content-center">
+        <div class="text-center">
+          <h1 class="font-black text-gray-200 text-9xl">500</h1>
+
+          <p class="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+            Uh-oh!
+          </p>
+
+          <p class="mt-4 text-gray-500">We can't find that page.</p>
+          <p class=" text-gray-500">Heading back home...</p>
+        </div>
+      </div>
     </div>
   );
-}
+};
+
+export default Custom500;
