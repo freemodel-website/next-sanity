7723320a3bca3400522de3b050cd009fe895b939
Nicholas Siciliano <nsicilia@oswego.edu>
2024-03-08 16:45:07 -0500

    white

---
+++ frontend/components/atoms/accordianlistwhite.js
@@ -0,0 +1,88 @@
+import { useRef, useState } from "react";
+
+const AccordianlistWhite = ({ faqsList }) => {
+  const answerElRef = useRef();
+  const [state, setState] = useState(false);
+  const [answerH, setAnswerH] = useState("0px");
+
+  const handleOpenAnswer = () => {
+    const answerElH = answerElRef.current.childNodes[0].offsetHeight;
+    setState(!state);
+    setAnswerH(`${answerElH + 20}px`);
+  };
+
+  const lines = faqsList.a.split("\n");
+
+  return (
+    <div
+      className="space-y-3 mt-5 overflow-hidden border-b "
+      key={faqsList.q}
+      onClick={handleOpenAnswer}
+    >
+      <h4
+        className={`cursor-pointer pb-5 flex items-center justify-between text-lg 
+      ${
+        state ? "text-FM-orange" : "text-white"
+      } font-bold hover:text-FM-orange`}
+      >
+        {faqsList.q}
+        {state ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5 text-white hover:text-FM-orange ml-2"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M20 12H4"
+            />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5 text-gray-500 ml-2 hover:text-FM-orange"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M12 4v16m8-8H4"
+            />
+          </svg>
+        )}
+      </h4>
+      <div
+        ref={answerElRef}
+        className="duration-300"
+        style={state ? { height: answerH } : { height: "0px" }}
+      >
+        <div>
+          {lines.map((line, index) => (
+            <p className="text-white" key={index}>
+              {line}
+            </p>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default function FaqSection({ faqsList }) {
+  return (
+    <section className="leading-relaxed max-w-screen-xl mx-auto px-4 md:px-8">
+      <div className="max-w-2xl mx-auto">
+        {faqsList.map((item) => (
+          <AccordianlistWhite key={item.a} faqsList={item} />
+        ))}
+      </div>
+    </section>
+  );
+}
--- frontend/components/designqa.js
+++ frontend/components/designqa.js
@@ -1,5 +1,6 @@
 import React from "react";
-import Accordianlist from "./atoms/accordianlist";
+
+import AccordianlistWhite from "./atoms/accordianlistwhite";
 import Ctabutton from "./atoms/ctabutton";
 import { urlFor } from "../client";
 
@@ -7,7 +8,7 @@ export default function DesignQA({ faqsList, image, buttontitle }) {
   return (
     <div className="bg-FM-blue">
       <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-        <h1 className="mb-1 text-4xl font-semibold tracking-wide text-center sm:text-5xl md:mb-20 ">
+        <h1 className="mb-1 text-4xl font-semibold tracking-wide text-center sm:text-5xl md:mb-20 text-white">
           Benefits of Partnering with Freemodel
         </h1>
         <div className="grid gap-12 row-gap-8 content-start lg:grid-cols-2">
@@ -19,7 +20,7 @@ export default function DesignQA({ faqsList, image, buttontitle }) {
             />
           </div>
           <div className="flex flex-col justify-center">
-            <Accordianlist faqsList={faqsList} />
+            <AccordianlistWhite faqsList={faqsList} />
           </div>
         </div>
         <div className="text-center mt-12">
