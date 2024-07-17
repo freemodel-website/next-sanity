abc6c00b217846c6d7cc3e22af6937597a0921b5
Nicholas Siciliano <nsicilia@oswego.edu>
2024-07-16 21:50:21 -0400

    added id to CTA Button

--- frontend/components/atoms/ctabutton.js
+++ frontend/components/atoms/ctabutton.js
@@ -9,7 +9,11 @@ import React from "react";
 export default function Ctabutton({ href, text }) {
   return (
     <div className="inline-block cursor-pointer py-3 px-6 bg-FM-orange hover:bg-orange-600 active:bg-gray-500 active:shadow-none rounded-lg shadow ">
-      <a href={href} className="text-lg text-center text-white ">
+      <a
+        href={href}
+        id="cta_contact_button"
+        className="text-lg text-center text-white "
+      >
         {text ? text : "Let's Talk"}
       </a>
     </div>
